751a836f251eb436dd43e1493506dd0c673b91da
AimanFadil <aimanbello2001@gmail.com>
2023-12-05 16:07:23 +0100

    push di controllo

--- js/main.js
+++ js/main.js
@@ -21,3 +21,51 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+/* let precedente = document.querySelector('.prev');
+let successivo = document.querySelector('.next');
+
+precedente.addEventListener('click', function() {
+       
+    console.log('Elemento con classe "prev" cliccato');
+});
+
+successivo.addEventListener('click', function() {
+        
+    console.log('Elemento con classe "next" cliccato');
+}); */
+
+let imgCorrente = 0;
+
+const items = document.getElementsByClassName('items')
+function mostraImgCorrente() {
+    const stampa = document.createElement('div')
+    stampa.classList.add('item active')
+    
+
+    let stampaImgTesto = `<div class="position-absolute bottom-50 text-right color-white padding-text"><h2>${images.title}</h2><h5>${images.text}</h5></div>`
+    stampa.innerHTML = stampaImgTesto
+
+    console.log('Immagine corrente:', images[imgCorrente]);
+}
+
+let precedente = document.querySelector('.prev');
+let successivo = document.querySelector('.next');
+
+precedente.addEventListener('click', function() {
+    imgCorrente--;
+    if (imgCorrente < 0) {
+        imgCorrente = images.length - 1;
+    }
+    mostraImgCorrente();
+});
+
+successivo.addEventListener('click', function() {
+    imgCorrente++;
+    if (imgCorrente >= images.length) {
+        imgCorrente = 0;
+    }
+    mostraImgCorrente();
+});
+
+
